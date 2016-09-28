ce76257a06a940fcd21d9b1a3dff03bb5ed126ac
jar3b <hellotan@live.ru>
2016-09-28 13:37:17 +0300

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bottle-oop-rest',
-    version='0.0.4',
+    version='0.0.5',
     packages=['borest'],
     url='https://github.com/jar3b/bottle-oop-rest',
     license='MIT',
